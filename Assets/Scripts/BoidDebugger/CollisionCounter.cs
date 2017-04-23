92175d014bed5be885a8120d92322fb60737fed0
PavelDrabek <pazzicz@gmail.com>
2017-04-23 19:34:46 +0200

    Test02 + collision counter

---
+++ Assets/Scripts/BoidDebugger/CollisionCounter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionCounter : MonoBehaviour {
+
+	public static int collisions;
+	private static float timeSinceStart;
+
+	// Use this for initialization
+	void Start () {
+		collisions = 0;
+		timeSinceStart = Time.realtimeSinceStartup;
+	}
+	
+	void OnTriggerEnter(Collider c) {
+		collisions++;
+		Debug.Log("Collision");
+	}
+
+	void OnGUI() {
+		GUI.color = Color.black;
+		GUI.Label(new Rect(20, 20, 200, 20), "collisions/s: " + (int)(collisions / (Time.realtimeSinceStartup - timeSinceStart)) + ", count: " + collisions);
+	}
+}
